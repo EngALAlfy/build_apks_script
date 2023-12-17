bbd46f660d6ef689f9afd180b12279ce7bf52e1f
islam alalfy <islamalalfy.2020@azhar.edu.eg>
2023-12-17 18:06:31 +0200

    add mega

--- modules/git_module.py
+++ modules/git_module.py
@@ -9,16 +9,30 @@ import os
 
 
 def git_clone(project):
-    workspace_dir = os.getenv('GIT_WORKSPACE')
-    os.chdir(workspace_dir)
+    use_token = os.getenv('GIT_USE_TOKEN') == "true"
 
-    # clean any repo first
-    os.system(f"rmdir /s /q {project}")
+    workspace_dir = os.getenv('GIT_WORKSPACE')
+
+    project_path = f"{workspace_dir}\\{project}"
 
     user = os.getenv('GIT_USER')
     token = os.getenv('GIT_TOKEN')
     branch = os.getenv('GIT_BRANCH')
 
-    git_clone_command = f"git clone --single-branch --branch {branch} https://{user}:{token}@github.com/{user}/{project}.git"
+    os.chdir(project_path)
+
+    # check if the repo exist
+    if os.path.isdir(project_path) and os.listdir(project_path):
+        # run restore first
+        git_clone_command = f"git restore ."
+        os.system(git_clone_command)
+        # run pull from branch
+        git_clone_command = f"git pull origin {branch}:{branch}"
+    else:
+        if use_token:
+            git_clone_command = f"git clone --single-branch --branch {branch} https://{user}:{token}@github.com/{user}/{project}.git ."
+        else:
+            git_clone_command = f"git clone --single-branch --branch {branch} https://github.com/{user}/{project}.git ."
+
     os.system(git_clone_command)
-    os.chdir(f"{workspace_dir}\\{project}")
+
--- modules/upload_module.py
+++ modules/upload_module.py
@@ -5,3 +5,25 @@
 #           on upload files and folders      #
 #                                            #
 ##############################################
+import os
+
+from mega import Mega
+
+
+def upload_to_mega(project):
+    mega = Mega()
+
+    user = os.getenv('GIT_USER')
+    workspace_dir = os.getenv('GIT_WORKSPACE')
+    project_path = f"{workspace_dir}\\{project}"
+
+    email = os.getenv('GIT_WORKSPACE')
+    password = os.getenv('GIT_WORKSPACE')
+    m = mega.login(email, password)
+
+    # Specify the target folder on MEGA where you want to upload the file
+    mega_folder = f'{user}/{project}'
+
+    # Upload the file
+    file = m.upload(project_path, dest_path=mega_folder)
+    
