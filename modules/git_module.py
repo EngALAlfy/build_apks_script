9d39a471cb5bdb8ad48eaa08375922cb58668748
islam alalfy <islamalalfy.2020@azhar.edu.eg>
2023-12-25 12:32:10 +0200

    fixes

--- modules/git_module.py
+++ modules/git_module.py
@@ -21,22 +21,26 @@ def git_clone(project):
     user = os.getenv('GIT_USER')
     token = os.getenv('GIT_TOKEN')
     branch = os.getenv('GIT_BRANCH')
-
-    os.chdir(project_path)
-
+    
+    os.chdir(workspace_dir)
+    
     # check if the repo exist
     if os.path.isdir(project_path) and os.listdir(project_path):
+        os.chdir(project_path)
         # run restore first
         git_clone_command = f"git restore ."
         os.system(git_clone_command)
         # run pull from branch
         git_clone_command = f"git pull origin {branch}:{branch}"
+        os.system(git_clone_command)
     else:
         if use_token:
-            git_clone_command = f"git clone --single-branch --branch {branch} https://{user}:{token}@github.com/{user}/{project}.git ."
+            git_clone_command = f"git clone --single-branch --branch {branch} https://{user}:{token}@github.com/{user}/{project}.git"
         else:
-            git_clone_command = f"git clone --single-branch --branch {branch} https://github.com/{user}/{project}.git ."
-
-    os.system(git_clone_command)
+            git_clone_command = f"git clone --single-branch --branch {branch} https://github.com/{user}/{project}.git"
+        os.system(git_clone_command)
+        os.chdir(project_path)
+          
+        
     print(print_utils.warning(f"[{project}] Done git task"))
 
